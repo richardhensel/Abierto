20f363e8b06f2e3e6bd3cbb1e12a89fc8dd0d2e0
richardhensel <richardhensel20@gmail.com>
2018-02-14 22:21:38 +1000

    added arduino unlock sketch

---
+++ Arduino/Unlock/Unlock.ino
@@ -0,0 +1,25 @@
+
+
+#include <Servo.h>
+
+Servo slewServo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  slewServo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    slewServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    slewServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
