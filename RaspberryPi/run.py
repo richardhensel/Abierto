eab325dcfeec380350a2e7f6c38e8cf171c5749d
richardhensel <richardhensel@gmail.com>
2018-02-14 12:18:46 +0000

    completed raspberry pi polling and gpio control

--- RaspberryPi/run.py
+++ RaspberryPi/run.py
@@ -1,25 +1,37 @@
-
+import time
 import requests
 
-run = True
-queryString = "http:// "
+from gpiozero import LED
 
-unlockString="unlock"
-lockString="lock"
+
+run = True
+queryString = "http://128.199.90.5/get/open"
+
+unlockString="1"
+lockString="0"
+
+led = LED(2)
 
 while run == True:
 
     # query the api
     response = requests.get(queryString)
 
-    if response.status_code == 200 and response == unlockString:
+    if response.status_code == 200 and response.text == unlockString:
+
+        print "unlock"
+        print "gpio high"
+        led.on()
         #set gpio to high
-        sleep(1)
+        time.sleep(1)
+
+        print "gpio low"
+        led.off()
         #set gpio to low
-        sleep(10)
+        time.sleep(10)
 
     else:
-        sleep(2)
+        time.sleep(2)
     
 
 
